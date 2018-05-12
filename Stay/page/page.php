e7c69b436716d738d8545197afe99a7a58a4f657
Ysnv <100954636@qq.com>
2018-05-12 18:36:19 +0800

    Revert "Revert "Add files via upload""
    This reverts commit 0b5a4f4d08da6d3d16b060835bac40cabf2f10d9.

---
+++ Stay/page/page.php
@@ -0,0 +1,58 @@
+<?php /*
+Template Name: 文章模板
+author: Stay Ma
+url: http://www.stayma.cn
+*/
+?>
+<?php get_header(); ?>
+    <div id="Tbody">
+    <?php if( have_posts() ){ the_post();?>
+        <div class="headerFilter" style="background:<?php echo stripslashes(get_option('stay_color'));?> <?php if(!empty(stripslashes(get_option('stay_topBg')))):?>;background: url('<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url();  } else { echo stripslashes(get_option('stay_topBg'));} ?>') <?php endif;?>">
+            <div class="headerFilterZ">
+                <div class="blogName artTitle">
+                    <p  style="color:<?php echo stripslashes(get_option('stay_title_color')); ?>;"><?php the_title(); ?></p>
+                    <span  style="color:<?php echo stripslashes(get_option('stay_Notice_color')); ?>;"><?php echo stripslashes(get_option('stay_Notice')); ?></span>
+                </div>
+            </div>
+        </div>
+        <div class="main">
+            <div class="hestiaBlogs">
+                <div class="container">
+                    <div class="blogsPost">
+                        <div class="artPadd">
+                            <article class="article artText">
+                                    <?php the_content(); ?>
+                            </article>
+                            <footer class="artFootInfo">
+                                <!-- 文章尾部 -->
+                                <p># <?php the_time('Y-n-j ') ?></p>
+                                <p id="share"><a href="javascript:;"><i class="fa fa-share-square"></i> 转发 </a></p>
+                                <p id="arrows"><a href="javascript:;"><i class="fa fa-arrows-v"></i> 隐藏评论 </a></p>
+                            </footer>
+                        </div>
+                        <?php } ?>
+                        <div class="comments-div">
+                            <div id="visible">
+                                <div class="visible">
+                                        <a target="blank" href="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/visible/qzone.png" alt=""></a>
+                                        <a target="blank" href="http://service.weibo.com/share/share.php?title=<?php the_title(); ?>&url=<?php the_permalink(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/visible/weibo.png" alt=""></a>
+                                        <a target="blank" href="http://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_title(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/visible/renren.png" alt=""></a>
+                                        <a target="blank" href="http://www.douban.com/recommend/?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/visible/douban.png" alt=""></a>
+                                </div>
+                            </div>
+
+                            <div class="comments">
+                                <?php comments_template() ;?>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- blogSidebar -->
+        <?php get_sidebar(); ?>
+                </div>
+            </div>
+    <?php get_footer(); ?>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Stay/sidebar.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @Author: Alamr
+ * @Date:   2018-05-05 14:15:23
+ * @Last Modified by:   Alamr
+ * @Last Modified time: 2018-05-09 21:19:59
+ */
+?>
+<!-- blogSidebar -->
+<div class="blogSidebar">
+    <!-- 搜索 -->
+    <form class="Alaeym-header-search" action="<?php echo home_url( '/' ); ?>" method="GET">
+      <span class="input input--haruki">
+            <input class="input__field input__field--haruki" type="text" id="input-1" name="s" />
+            <label class="input__label input__label--haruki" for="input-1">
+                <span class="input__label-content input__label-content--haruki">搜索一下(回车确认)</span>
+            </label>
+        </span>
+    </form>
+    <!-- 热门文章 -->
+    <div class="topArticle">
+    <h2><i class="fa fa-signal"></i> Top  List</h2>
+        <ul>
+        <?php
+        $post_num = 10; // 设置调用条数
+        $args = array(
+        'post_password' => '',
+        'post_status' => 'publish',
+        'post__not_in' => array($post->ID),
+        'caller_get_posts' => '1',
+        'orderby' => 'comment_count',
+        'posts_per_page' => $post_num
+        );
+        $query_posts = new WP_Query();
+        $query_posts->query($args);
+        while( $query_posts->have_posts() ) { $query_posts->the_post(); ?>
+        <li>
+        <a href='<?php the_permalink(); ?>' title='<?php the_title(); ?>'># <?php echo wp_trim_words( get_the_title(), 15 ); ?></a>
+          <p><?php get_post_views($post->ID); ?>次围观</p>
+        </li>
+        <?php } wp_reset_query();?>
+        </ul>
+    </div>
+</div>
